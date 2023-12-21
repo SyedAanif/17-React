df1ce0a6c3efeac1bc1e13c93ad1635234132eef
Syed Aanif <syedaanif07@gmail.com>
2023-12-21 10:36:56 +0530

    MAP data to components

--- 10-contacts_app_proj/src/components/App.js
+++ 10-contacts_app_proj/src/components/App.js
@@ -2,20 +2,28 @@ import React from "react";
 import CardComponent from "./Card";
 import contacts from "../contacts";
 
+function createCard(contact) {
+  // NOTE:: Each mapped element must have a unique id(React Virtual DOM)
+  // Thus add a "key" field
+  return (
+    <CardComponent
+      // this is for the uniqueness of the component and is internally used by React
+      key={contact.id}
+      name={contact.name}
+      imgURL={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      {contacts.map((contact) => {
-        return (
-          <CardComponent
-            name={contact.name}
-            imgURL={contact.imgURL}
-            phone={contact.phone}
-            email={contact.email}
-          />
-        );
-      })}
+      {/* converted anonymous function into named function
+      Maps each element in the loop to object */}
+      {contacts.map(createCard)}
     </div>
   );
 }
